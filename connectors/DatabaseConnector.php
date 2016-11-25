68cc5874c90f5e15d671b63b66074e86d8943def
shmilyzxt <49783121@qq.com>
2016-11-25 17:37:20 +0800

    redis队列支持

---
+++ connectors/DatabaseConnector.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 数据库连接器（使用yii的）
+ * User: shmilyzxt 49783121@qq.com
+ * Date: 2016/11/25
+ * Time: 17:35
+ */
+
+namespace shmilyzxt\queue\connectors;
+
+
+use yii\base\Component;
+
+class DatabaseConnector extends Component
+{
+
+    public function  init()
+    {
+        parent::init();
+        throw new \Exception('you should use yii\db\Connection as the database connector!');
+    }
+}
